f1a3037cf8764886aba9163405fa1edbe554c4c9
Juan David Franco <jfrancoh5@gmail.com>
2024-06-04 06:23:03 -0500

    metodo de registro de usuarios - parte 2

--- controllers/user.js
+++ controllers/user.js
@@ -9,9 +9,20 @@ const pruebaUser = (req,res) =>{
 
 //registro
 const register = (req,res) =>{
+    //recoger los datos de la petición
+    let params = req.body;
+
+    //comprobar que los datos llegan bien
+    if(!params.name || !params.nickname || !params.email || !params.password){
+        return res.status(400).json({
+            status:"failed",
+            message: "Faltan datos por enviar"
+        })
+    }
+
     return res.status(200).json({
         status:"success",
-        message:"Usuario creado exitosamente"
+        message:"Usuario creado exitosamente",
     })
 }
 
