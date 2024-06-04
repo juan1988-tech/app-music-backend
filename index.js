9968bc8f32e60b3e29068ee6b775ee94ecbb8eae
Juan David Franco <jfrancoh5@gmail.com>
2024-06-03 19:20:56 -0500

    crear el servidor de node

--- index.js
+++ index.js
@@ -2,3 +2,25 @@
 const connection = require('./database/connection');
 
 connection()
+//importar dependencias
+const express = require('express');
+const cors = require('cors');
+
+console.log('Bienvenido al backend de la aplicación de música')
+
+//crear un servidor en node
+
+const app = express();
+const port = 3009
+
+//arrancar el cors
+app.use(cors());
+
+//arrancar el método para convertir archivos a objeto json
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+//poner el servidor a escuchar
+app.listen(port, ()=>{
+    console.log('servidor de node esta escuchando en el puerto ' + port)
+}) 
