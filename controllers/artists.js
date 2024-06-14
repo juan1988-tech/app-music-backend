5c8137b82afea886e950ff4aab292f044f0504af
Juan David Franco <jfrancoh5@gmail.com>
2024-06-13 19:09:38 -0500

    Accion sacar listado de artistas

--- controllers/artists.js
+++ controllers/artists.js
@@ -54,4 +54,38 @@ const oneArtist = async (req,res) =>{
     })
 }
 
-module.exports = { pruebaArtist, save, oneArtist}
+const list = (req,res) =>{
+    //sacar la posible página
+    let page = 1;
+
+    if(req.params.page){
+        page = req.params.page;
+    }
+
+    //numero de elementos por página
+    const itemsPage = 5;
+
+    //retorna a varios artistas
+    Artist.find().exec().then((artistas)=>{
+
+        //array para organizar a la lista de artistas por orden alfabetico
+        let artistasOrdenados =  artistas.sort((a,b)=>{
+            let x = a.name;
+            let y = b.name;
+            if( x < y ){ return -1};
+            if( x > y ){ return 1 };
+            return 0
+        })
+
+
+        return res.status(200).json({
+            status:"success",
+            message:"Listado de artistas",
+            page,
+            artists: artistasOrdenados
+        })
+    })
+    
+}
+
+module.exports = { pruebaArtist, save, oneArtist, list }
--- routes/artists.js
+++ routes/artists.js
@@ -12,5 +12,7 @@ const artistController =  require('../controllers/artists')
 router.get('/prueba',artistController.pruebaArtist)
 router.post('/save',check.auth,artistController.save)
 router.get('/ver-artista/:id',check.auth,artistController.oneArtist)
+router.get('/ver-lista-de-artistas/:page?',check.auth,artistController.list)
+
 //exportar la ruta
 module.exports = router;
