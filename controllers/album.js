d8463f7f8067710337d9def7974aec1c478a7556
Juan David Franco <jfrancoh5@gmail.com>
2024-06-17 17:19:58 -0500

    modelo de album

--- controllers/album.js
+++ controllers/album.js
@@ -1,9 +1,14 @@
-const pruebaAlbum = (req,res) =>{
+const Album = require('../models/album');
 
+const pruebaAlbum = (req,res) =>{
+    console.log(Album)
+    
     return res.status(200).send({
         status: "success",
         message:"Mensaje enviado desde el archivo album.js"
     })
 }
+
+
 
 module.exports = { pruebaAlbum }
